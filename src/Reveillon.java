1afe05e91b396b63ada43ea5f9aa5357c2417b12
PandaGotico <pvictor2112@hotmail.com>
2021-12-10 10:25:55 -0300

    Adição de seletor de datas

--- src/Reveillon.java
+++ src/Reveillon.java
@@ -18,21 +18,34 @@ public class Reveillon {
     private static final List<String> MES_DEZEMBRO = List.of("DEZEMBRO", "DEZ", "12");
     private static final List<String> MES_30 = List.of("4","6","9","11");
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int dia;
+        int escolha;
+        int dia, outroDia;
         int mes = 0;
+        int outroMes = 0;
+        int ano, outroAno;
         int diasRestantes = 365;
-        String mesEscrito;
+        int diferencaDiaMes = 0;
+        int diaMes = 0;
+        int diaOutroMes = 0;
+        int difAno = 0;
+        String mesEscrito, outroMesEscrito;
 
-        System.err.println("informe o dia de hoje: ");
-        dia = scan.nextInt();
+        System.out.println("Escolha uma das opções:");
+        System.out.println("Final do ano = 1");
+        System.out.println("Data especifica = 2");
+        escolha = scan.nextInt();
 
-        System.out.println("informe o mês: ");
-        mesEscrito = scan.next().toUpperCase(Locale.ROOT);
+        if (escolha == 1){
+            System.out.println("informe o dia de hoje: ");
+            dia = scan.nextInt();
 
-        //condição do valor do mês//
+            System.out.println("informe o mês: ");
+            mesEscrito = scan.next().toUpperCase(Locale.ROOT);
+
+            //condição do valor do mês//
 
             if (MES_JANEIRO.contains(mesEscrito)) {
                 mes = 1;
@@ -82,38 +95,253 @@ public class Reveillon {
                 mes = 12;
 
             } else {
+                System.err.println("Erro no valor do mês");
+            }
+            if (mes <= 2) {
+                if (dia > 31) {
+                    System.err.println("Data invalida!");
+
+                } else if (mes == 2 && dia == 29) {
+                    System.out.println("hoje é dia " + dia + "/" + mes);
+                    System.out.println("Faltam apenas " + (diasRestantes - dia + 1) + " para o ano novo.");
+
+                } else if (mes == 2 && dia >= 28 || dia <= 0) {
+                    System.err.println("Data invalida!");
+
+                } else if (MES_30.contains(mes) && dia <= 0 || dia > 30) {
+                    System.err.println("Data invalida!");
+
+                } else {
+                    System.out.println("Hoje é dia " + dia + "/" + mes);
+                    System.out.println("Faltam apenas " + (diasRestantes - dia) + " para o ano novo.");
+                    System.out.println("Se o ano for bisexto " + (diasRestantes - dia + 1) + ". :) ");
+                }
+            } else {
+                if (dia > 31) {
+                    System.err.println("Data invalida!");
+
+                } else if (MES_30.contains(mes) && dia <= 0 || dia > 30) {
+                    System.err.println("Data invalida!");
+
+                } else {
+                    System.out.println("Hoje é dia " + dia + "/" + mes);
+                    System.out.println("Faltam apenas " + (diasRestantes - dia) + " para o ano novo.");
+                }
+            }
+        }
+
+        else if (escolha == 2){
+            System.out.println("informe o dia de hoje: ");
+            dia = scan.nextInt();
+
+            System.out.println("informe o mês: ");
+            mesEscrito = scan.next().toUpperCase(Locale.ROOT);
+
+            System.out.println("informe o ano: ");
+            ano = scan.nextInt();
+
+            System.out.println("informe o próximo dia: ");
+            outroDia = scan.nextInt();
+
+            System.out.println("informe o próximo  mês: ");
+            outroMesEscrito = scan.next().toUpperCase(Locale.ROOT);
+
+            System.out.println("informe o próximo ano: ");
+            outroAno = scan.nextInt();
+
+            if (MES_JANEIRO.contains(mesEscrito)) {
+                diaMes = 0;
+                mes = 1;
+
+            } else if (MES_FEVEREIRO.contains(mesEscrito)) {
+                diaMes = 31;
+                diasRestantes = (365 - 31);
+                mes = 2;
+
+            } else if (MES_MARCO.contains(mesEscrito)) {
+                diaMes = 59;
+                diasRestantes = (365 - 59);
+                mes = 3;
+
+            } else if (MES_ABRIL.contains(mesEscrito)) {
+                diaMes = 90;
+                diasRestantes = (365 - 90);
+                mes = 4;
+
+            } else if (MES_MAIO.contains(mesEscrito)) {
+                diaMes = 120;
+                diasRestantes = (365 - 120);
+                mes = 5;
+
+            } else if (MES_JUNHO.contains(mesEscrito)) {
+                diaMes = 151;
+                diasRestantes = (365 - 151);
+                mes = 6;
+
+            } else if (MES_JULHO.contains(mesEscrito)) {
+                diaMes= 181;
+                diasRestantes = (365 - 181);
+                mes = 7;
+
+            } else if (MES_AGOSTO.contains(mesEscrito)) {
+                diaMes = 211;
+                diasRestantes = (365 - 211);
+                mes = 8;
+
+            } else if (MES_SETEMBRO.contains(mesEscrito)) {
+                diaMes = 242;
+                diasRestantes = (365 - 242);
+                mes = 9;
+
+            } else if (MES_OUTUBRO.contains(mesEscrito)) {
+                diaMes = 272;
+                diasRestantes = (365 - 272);
+                mes = 10;
+
+            } else if (MES_NOVEMBRO.contains(mesEscrito)) {
+                diaMes = 303;
+                diasRestantes = (365 - 303);
+                mes = 11;
+
+            } else if (MES_DEZEMBRO.contains(mesEscrito)) {
+                diaMes = 333;
+                diasRestantes = (365 - 333);
+                mes = 12;
+
+            } else {
                 System.out.println("Erro no valor do mês");
             }
-        if (mes <= 2) {
-            if (dia > 31) {
-                System.err.println("Data invalida!");
+            if (MES_JANEIRO.contains(outroMesEscrito)) {
+                diaOutroMes = 0;
+                outroMes = 1;
 
-            } else if (mes == 2 && dia == 29) {
-                System.out.println("hoje é dia " + dia + "/" + mes);
-                System.out.println("Faltam apenas " + (diasRestantes - dia + 1) + " para o ano novo.");
+            } else if (MES_FEVEREIRO.contains(outroMesEscrito)) {
+                diaOutroMes = 31;
+                outroMes = 2;
 
-            } else if (mes == 2 && dia >= 28 || dia <= 0) {
-                System.err.println("Data invalida!");
+            } else if (MES_MARCO.contains(outroMesEscrito)) {
+                diaOutroMes = 59;
+                outroMes = 3;
 
-            } else if (MES_30.contains(mes) && dia <= 0 || dia > 30) {
-                System.err.println("Data invalida!");
+            } else if (MES_ABRIL.contains(outroMesEscrito)) {
+                diaOutroMes = 90;
+                outroMes = 4;
+
+            } else if (MES_MAIO.contains(outroMesEscrito)) {
+                diaOutroMes = 120;
+                outroMes = 5;
+
+            } else if (MES_JUNHO.contains(outroMesEscrito)) {
+                diaOutroMes = 151;
+                outroMes = 6;
+
+            } else if (MES_JULHO.contains(outroMesEscrito)) {
+                diaOutroMes = 181;
+                outroMes = 7;
+
+            } else if (MES_AGOSTO.contains(outroMesEscrito)) {
+                diaOutroMes = 211;
+                outroMes = 8;
+
+            } else if (MES_SETEMBRO.contains(outroMesEscrito)) {
+                diaOutroMes = 242;
+                outroMes = 9;
+
+            } else if (MES_OUTUBRO.contains(outroMesEscrito)) {
+                diaOutroMes = 272;
+                outroMes = 10;
+
+            } else if (MES_NOVEMBRO.contains(outroMesEscrito)) {
+                diaOutroMes = 303;
+                outroMes = 11;
+
+            } else if (MES_DEZEMBRO.contains(outroMesEscrito)) {
+                diaOutroMes = 333;
+                outroMes = 12;
 
             } else {
-                System.out.println("Hoje é dia " + dia + "/" + mes);
-                System.out.println("Faltam apenas " + (diasRestantes - dia) + " para o ano novo.");
-                System.out.println("Se o ano for bisexto " + (diasRestantes - dia + 1) + ". :) ");
+                System.out.println("Erro no valor do mês");
             }
-        } else {
-            if (dia > 31) {
-                System.err.println("Data invalida!");
 
-            }  else if (MES_30.contains(mes) && dia <= 0 || dia > 30) {
-                System.err.println("Data invalida!");
+            if(outroAno < ano) {
+                System.err.println("Valor invalido");
 
-            } else {
-                System.out.println("Hoje é dia " + dia + "/" + mes);
-                System.out.println("Faltam apenas " + (diasRestantes - dia) + " para o ano novo.");
-            }
+            } else if (ano == outroAno) {
+                diferencaDiaMes = ((outroDia + diaOutroMes) - (dia + diaMes));
+
+                if(outroMes < mes) {
+                    System.err.println("valor invalido");
+                } else if (mes <= 2) {
+                    if (dia > 31) {
+                        System.err.println("Data invalida!");
+
+                    } else if (mes == 2 && dia == 29) {
+                        System.out.println("hoje é dia " + dia + "/" + mes);
+                        System.out.println("Faltam apenas " + (diferencaDiaMes + 1) + " a data selecionada.");
+
+                    } else if (mes == 2 && dia >= 28 || dia <= 0) {
+                        System.err.println("Data invalida!");
+
+                    } else if (MES_30.contains(mes) && dia <= 0 || dia > 30) {
+                        System.err.println("Data invalida!");
+
+                    } else {
+                        System.out.println("Hoje é dia " + dia + "/" + mes);
+                        System.out.println("Faltam apenas " + (diferencaDiaMes) + " a data selecionada.");
+                        System.out.println("Se o ano for bisexto " + (diferencaDiaMes + 1) + ". :) ");
+                    }
+                } else {
+                    if (dia > 31) {
+                        System.err.println("Data invalida!");
+
+                    } else if (MES_30.contains(mes) && dia <= 0 || dia > 30) {
+                        System.err.println("Data invalida!");
+
+                    } else {
+                        System.out.println("Hoje é dia " + dia + "/" + mes);
+                        System.out.println("Faltam apenas " + (diferencaDiaMes) + " a data selecionada.");
+                    }
+                }
+            } else if (outroAno > ano) {
+                difAno = (((outroAno - ano) -1) * 365);
+                diferencaDiaMes = ((diasRestantes - dia) + (diaOutroMes + outroDia) + difAno);
+
+                if (mes <= 2) {
+                    if (dia > 31) {
+                        System.err.println("Data invalida!");
+
+                    } else if (mes == 2 && dia == 29) {
+                        System.out.println("hoje é dia " + dia + "/" + mes);
+                        System.out.println("Faltam apenas " + (diferencaDiaMes + 1) + " a data selecionada.");
+
+                    } else if (mes == 2 && dia >= 28 || dia <= 0) {
+                        System.err.println("Data invalida!");
+
+                    } else if (MES_30.contains(mes) && dia <= 0 || dia > 30) {
+                        System.err.println("Data invalida!");
+
+                    } else {
+                        System.out.println("Hoje é dia " + dia + "/" + mes);
+                        System.out.println("Faltam apenas " + (diferencaDiaMes) + " a data selecionada.");
+                        System.out.println("Se o ano for bisexto " + (diferencaDiaMes + 1) + ". :) ");
+                    }
+                } else if(outroMes > 2){
+                    System.out.println("Hoje é dia " + dia + "/" + mes);
+                    System.out.println("Faltam apenas " + (diferencaDiaMes) + " a data selecionada.");
+                    System.out.println("Se o ano for bisexto " + (diferencaDiaMes + 1) + ". :) ");
+                } else {
+                    if (dia > 31) {
+                        System.err.println("Data invalida!");
+
+                    } else if (MES_30.contains(mes) && dia <= 0 || dia > 30) {
+                        System.err.println("Data invalida!");
+
+                    } else {
+                        System.out.println("Faltam apenas " + (diferencaDiaMes) + " a data selecionada.");
+                    }
+                }
+            } else
+                System.err.println("Valor invalido");
         }
     }
 }
